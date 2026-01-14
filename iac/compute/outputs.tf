c5ea2fbd8ac0c51ec5d3b986b4072e65dbf0cb12
phdsystems <developer@phdsystems.com>
2026-01-14 07:53:55 +0200

    Remove old resource-first directories
    Delete obsolete structure after migrating to provider-first pattern
    
    Removed:
    - iac/compute/ (was resource-first, now in iac_core/aws/compute/)
    - iac/storage/ (was resource-first, now in iac_core/aws/storage/)
    - iac/database/ (was resource-first, now in iac_core/aws/database/)
    - iac/networking/ (was resource-first, now in iac_core/aws/networking/)
    - iac/iam/ (was resource-first, now in iac_core/aws/iam/)
    
    Old Pattern (Wrong):
      iac/compute/aws/  <- Resource type first
      iac/storage/aws/  <- Resource type first
    
    New Pattern (Correct):
      iac_core/aws/compute/  <- Provider first (matches CloudKit)
      iac_core/aws/storage/  <- Provider first (matches CloudKit)
    
    Clean structure maintained

--- iac/compute/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "instance_name" {
-  description = "The name of the provisioned compute instance."
-  value       = var.instance_name
-}
-
-output "selected_provider" {
-  description = "The cloud provider that was used to provision the resource."
-  value       = var.provider
-}
