2c7e03bb2bbef10cef2d73ff25c36792a93d1f39
phdsystems <developer@phdsystems.com>
2026-01-14 14:35:24 +0200

    refactor: remove ui applications

--- frontend/src/components/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod sidebar;
-pub mod service_list;
-pub mod resource_tree;
-pub mod log_viewer;
-pub mod request_table;
--- frontend/src/pages/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod dashboard;
-pub mod explorer;
-pub mod providers;
-pub mod inspector;
-pub mod settings;
